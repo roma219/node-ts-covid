8a1a8c00879b9525da90ab306dba15d03957bb1f
Roman Sabirov <roma219@yandex.ru>
2020-04-14 17:19:04 +0800

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,66 @@
+import express, { Request, Response, NextFunction } from 'express'
+import fetch from 'node-fetch';
+import { fetchStats } from './fetchStats'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+const app = express()
+
+app.use(express.json())
+
+type User = {
+    login: string
+    password: string
+}
+
+// use db
+const users : User[] = []
+
+app.get('/stats', checkAuth, async (req, res) => {
+    const stats = await fetchStats()
+    res.send(stats)
+})
+
+app.post('/login', (req, res) => {
+    const { login, password } = req.body
+
+    if (!login || !password) return res.sendStatus(400)
+
+    const user = users.find(item => item.login === login)
+
+    if (!user) return res.sendStatus(400)
+
+    bcrypt.compare(password, user.password, (err, result) => {
+        if (err || !result) return res.sendStatus(401)
+        const token = jwt.sign({ login }, 'kek')
+        res.send({ token })
+    });
+})
+
+app.post('/signup', (req, res) => {
+    const { login, password } = req.body
+    if (!login || !password || users.find(item => item.login === login))
+        return res.status(400).send('Invalid data provided')
+
+    const hashedPassword = bcrypt.hash(password, 10, (err, hash) => {
+        if (err) return res.sendStatus(500)
+        users.push({ login, password: hash })
+        res.status(201).send()
+    })
+})
+
+function checkAuth(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.token as string
+    if (!authHeader) return res.sendStatus(401)
+
+    const token = authHeader.split(' ')[1]
+    if (!token) res.sendStatus(401)
+
+    jwt.verify(token, 'kek', (err, decoded) => {
+        if (err) return res.sendStatus(403)
+        next()
+    })
+}
+
+// use env for port
+app.listen(5000, () => console.log('Server running on port 5000'))
---
+++ src/fetchStats.ts
@@ -0,0 +1,43 @@
+import fetch from 'node-fetch'
+import cheerio from 'cheerio'
+import tableparser from 'cheerio-tableparser'
+
+type CountryData = {
+    name: string
+    totalCases: number
+    totalDeaths: number
+}
+
+enum TableFields {'Country,Other', 'TotalCases', 'TotalDeaths'}
+
+enum DataFields {
+    'Country,Other' = 'name',
+    'TotalCases' = 'totalCases',
+    'TotalDeaths' = 'totalDeaths',
+}
+
+const fetchStats = () => {
+    return fetch('https://www.worldometers.info/coronavirus/')
+        .then(res => res.text())
+        .then(body => {
+            const $ = cheerio.load(body)
+            tableparser($)
+            const result = $('.maincounter-number').text()
+            const data = ($('#main_table_countries_today') as any).parsetable(true, true, true)
+
+
+            const info = data.filter((row : string[]) => Object.values(TableFields).includes(row[0])).reduce((result : CountryData[], current: string[]) => {
+                current.forEach((value, index) => {
+                    if (!result[index]) result[index] = { 'name': '', 'totalCases': 0, 'totalDeaths': 0 }
+                    ;(result[index] as any)[(DataFields as any)[current[0]]] = current[index]
+                })
+                return result
+            }, [])
+
+            info.shift()
+
+            return info
+        });
+}
+
+export { fetchStats }
