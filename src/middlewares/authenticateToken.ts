0cafa8b63143ade66f64cd88d03cda8793469267
Roman Sabirov <roma219@yandex.ru>
2020-04-16 14:23:21 +0800

    cleanup

--- src/middlewares/authenticateToken.ts
+++ src/middlewares/authenticateToken.ts
@@ -1,16 +1,15 @@
 import jwt from 'jsonwebtoken'
 import { RequestHandler } from 'express'
+import { UNAUTHORIZED, FORBIDDEN } from 'http-status-codes'
 import { User } from '../types'
 
 export const authenticateToken : RequestHandler = (req, res, next) => {
-  console.log('kek')
   const authHeader = req.headers.authorization as string || ''
-  console.log(req.headers)
   const token = authHeader.split(' ')[1]
-  if (!token) return res.sendStatus(401)
+  if (!token) return res.sendStatus(UNAUTHORIZED)
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, decoded) => {
-    if (err) return res.sendStatus(403)
+    if (err) return res.sendStatus(FORBIDDEN)
     const decodedLogin = (decoded as User)?.login
     req.user = decodedLogin
     next()
