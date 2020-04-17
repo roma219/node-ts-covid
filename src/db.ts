92535f8004ba115dc8587a72b6a39a88315ce00c
Roman Sabirov <roma219@yandex.ru>
2020-04-17 14:45:11 +0800

    add mongodb url

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const url = 'mongodb://127.0.0.1:27017/users'
+const url = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/users'
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.set('useCreateIndex', true)
